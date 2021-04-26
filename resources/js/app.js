7334d4033557795ae4b8abe6bd62cdab91298f14
Bruno Roberto Gomes <bruno@brgomes.com>
2021-04-26 16:02:48 -0300

    Envia mensagens

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,13 +1,30 @@
 require('./bootstrap');
 
 // Import modules...
-import { createApp, h } from 'vue';
+import { createApp, h, ref } from 'vue';
 import { App as InertiaApp, plugin as InertiaPlugin } from '@inertiajs/inertia-vue3';
 import { InertiaProgress } from '@inertiajs/progress';
+import moment from 'moment';
+
+moment.locale("pt-br");
 
 const el = document.getElementById('app');
 
+const app = createApp(InertiaApp)
+
+app.config.globalProperties.$filters = {
+    formatDate(value) {
+        if (value) {
+            return moment(value).format('dd/mm/YYYY HH:mm');
+        }
+    }
+}
+
 createApp({
+    setup() {
+        const filterDate = ref('');
+        return { filterDate }
+    },
     render: () =>
         h(InertiaApp, {
             initialPage: JSON.parse(el.dataset.page),
